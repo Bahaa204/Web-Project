de5823516a8d9e54be1ce6bae9d3996599c5f626
osama-ayoub <oso.ayb09812@gmail.com>
2025-11-25 16:23:07 +0200

    besmellah 3

--- Assets/JS/contact.js
+++ Assets/JS/contact.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
     let form = document.querySelector(".contact-form form");
     let modal = document.createElement("div");
 
-    // Create the modal structure
     modal.classList.add("modal");
     modal.innerHTML = `
         <div class="modal-content">
@@ -12,26 +11,23 @@ document.addEventListener("DOMContentLoaded", () => {
     `;
     document.body.appendChild(modal);
 
-    // Handle form submission
     form.addEventListener("submit", (event) => {
         event.preventDefault();
         modal.style.display = "flex";
         modal.classList.add("fade-in");
     });
 
-    // Close the modal
     let closeModal = () => {
         modal.classList.remove("fade-in");
         modal.classList.add("fade-out");
         setTimeout(() => {
             modal.style.display = "none";
             modal.classList.remove("fade-out");
-        }, 500); // Match the fade-out animation duration
+        }, 500);
     };
 
     modal.querySelector(".close-modal").addEventListener("click", closeModal);
 
-    // Close modal when clicking outside the content
     modal.addEventListener("click", (event) => {
         if (event.target === modal) {
             closeModal();
--- Assets/Scripts/home.js
+++ Assets/Scripts/home.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     $.getJSON("./Assets/Scripts/Data.json", function (data) {
-        let randomRecipes = data.sort(() => 0.5 - Math.random()).slice(0, 4); // Get 4 random recipes
+        let randomRecipes = data.sort(() => 0.5 - Math.random()).slice(0, 4);
 
         $(".recipe-card").each(function (index) {
             let recipe = randomRecipes[index];
